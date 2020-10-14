bebcad88f3ed3b658537812899c9441326b8d0d5
PeNalu <69108602+PeNalu@users.noreply.github.com>
2020-10-14 17:09:55 +0300

    PeNaLu
    Почти завершил вёрстку

--- dev 2/html/script.js
+++ dev 2/html/script.js
@@ -4,6 +4,12 @@ function addMessege()
 
 	var b = "<div class=\"media media-chat media-chat-reverse\"> <div class=\"media-body\"> <p>" + a + "</p> <p class=\"meta\"><time datetime=\"2018\">00:06</time></p> </div> </div>"
 
-	$("messege").append(b);
+	$("#messege").prepend(b);
 
+	document.getElementById("inputmessege").value = "";
 }
+
+
+// $("form").on("submit", function(){
+// 	addMessege();
+// })
